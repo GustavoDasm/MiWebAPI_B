a644918374d780f51406483e05ec612a79f8d2cd
Gustavo <animex36990@gmail.com>
2024-07-18 13:59:25 -0500

    Agregar archivos de proyecto.

---
+++ MiWebAPI/Models/EmpleadoSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace MyWebAPI.Models
+{
+    public class EmpleadoSettings : IEmpleadoSettings
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Collections { get; set; }
+    }
+
+    public interface IEmpleadoSettings
+    {
+        string Server { get; set; }
+        string Database { get; set; }
+        string Collections { get; set; }
+    }
+}
